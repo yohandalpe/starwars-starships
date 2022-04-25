cc072bf0196b46817d848a1109e4e08a76c73ca3
Yohan Dalpatadu <yohandalpe@gmail.com>
2022-04-25 18:34:21 +0530

    Added fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+        ropasans: ["RopaSans", "sans-serif"],
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms")],
 };
