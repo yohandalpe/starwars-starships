88568a9f75e15bf6ae721e807fd7a79e619cd5c1
Yohan Dalpatadu <yohandalpe@gmail.com>
2022-04-26 22:32:43 +0530

    Wrapped logo with a href

--- components/header.js
+++ components/header.js
@@ -1,11 +1,20 @@
 import Image from "next/image";
+import Link from "next/link";
 import logo from "../public/images/starwars-logo.png";
 
 const Header = () => {
   return (
     <>
       <div className="container-header">
-        <Image src={logo} alt="StarWars" width={203} height={88} />
+        <Link href="/" passHref>
+          <Image
+            src={logo}
+            alt="StarWars"
+            width={203}
+            height={88}
+            className="hover:cursor-pointer"
+          />
+        </Link>
       </div>
     </>
   );
